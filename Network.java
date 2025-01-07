ab81577675b16a8f65b7dc7f11c40d02c05b79c9
Simon Levy <simon.levy01@post.runi.ac.il>
2025-01-07 02:13:28 +0200

    fixed mistake in toString

--- Network.java
+++ Network.java
@@ -108,7 +108,7 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "";
+        String ans = "Network: \n";
         for (int i = 0; i<userCount; i++){
         ans = ans + users[i].toString() + "\n";
        }
